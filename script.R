1757791c72d476509ce81139c518ef992b24eea2
Majid Einian <einian85@gmail.com>
2016-10-05 11:58:10 +0330

    update scrip.R to source files inside R folder
    update scrip.R to source files inside R folder after moving it out of
    the folder

--- script.R
+++ script.R
@@ -3,20 +3,20 @@
 #require(vars)
 #require(forecast)
 
-source("utls.R") 
-source("evaluation.R")
-source("ldt.R")
-source("ldtpack.R")
-source("ldtpacksub.R")
-source("ldtpackarimasub.R")
-source("ldtpackarima.R")
+source("R/utls.R")
+source("R/evaluation.R")
+source("R/ldt.R")
+source("R/ldtpack.R")
+source("R/ldtpacksub.R")
+source("R/ldtpackarimasub.R")
+source("R/ldtpackarima.R")
 
- 
+
 a = ldt$new()
 a$setData(data_file = "endodata.txt")
 a$setData(is_endo = FALSE, data_file = "exodata.txt")
 a$MaxSize = 3
 a$SimulationCount = 3
-a$setPacks() 
+a$setPacks()
 a$Run()
- 
+
